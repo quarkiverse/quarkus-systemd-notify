22885b6a606fb6629276589c35e3b68ce7a78ca8
George Gastaldi <gegastaldi@gmail.com>
2022-12-07 00:08:43 -0300

    Commit source changes after mvn clean package

--- deployment/src/test/java/io/quarkiverse/systemd/notify/test/SystemdNotifyTest.java
+++ deployment/src/test/java/io/quarkiverse/systemd/notify/test/SystemdNotifyTest.java
@@ -5,6 +5,7 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+
 import io.quarkus.test.QuarkusUnitTest;
 
 public class SystemdNotifyTest {
